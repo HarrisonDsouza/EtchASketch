ee2bff3d76d233002417b189ce36b39e3d44fa30
Harrison <harrisondsouza76@gmail.com>
2022-11-04 21:27:53 +0530

    Add gray-scale mode

--- script.js
+++ script.js
@@ -11,20 +11,28 @@ function createBoxes(count){
     boxes.forEach(box => {
         box.style.flexBasis = `${100/side}%`;
     })
-}
+
+    canvasSize.textContent = `${side}x${side}`;
+}   
 
 // paint boxes on click
 function color(option = 'black'){
     let boxes = document.querySelectorAll('main .container > *')
 
     let isDown = false;
-
-
+    
     for (let box of boxes){
+        let brightness = 100;
         box.addEventListener('mousedown', () => {
             if(isMixed){//checks if mixed color option is selected
                 option ="#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
             }
+            if(isGrayScale){//check if gray scale mode is selected
+                brightness -= 10;
+                option = `hsl(0, 0%, ${brightness}%)`;
+                
+            }
+
             box.style.backgroundColor = option;
             isDown = true;
         })
@@ -35,9 +43,15 @@ function color(option = 'black'){
         })
     }
     for (let box of boxes){
+        let brightness = 100;
         box.addEventListener('mouseover', () => {
             if(isMixed){//checks if mixed color option is selected
                 option ="#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+            }
+            if(isGrayScale){//check if gray scale mode is selected
+                brightness -= 10;
+                option = `hsl(0, 0%, ${brightness}%)`;
+                
             }
             if(isDown){
                 box.style.backgroundColor = option;
@@ -51,26 +65,41 @@ function color(option = 'black'){
 let black = document.querySelector('main .customization .black');
 let erase = document.querySelector('main .customization .erase');
 let mixed = document.querySelector('main .customization .mixed');
-let isMixed = false
+let grayScale = document.querySelector('main .customization .gray-scale');
+let isMixed = false;
+let isGrayScale = false;
 
 black.addEventListener('click', ()=>{
     color('black');
     isMixed = false;
+    isGrayScale = false;
 });
 
 mixed.addEventListener('click', () => {
     isMixed = true;
+    isGrayScale = false;
+});
+
+grayScale.addEventListener('click', () => {
+    isMixed = false;
+    isGrayScale = true;
+
 });
 
 erase.addEventListener('click', ()=>{
     color('transparent');
     isMixed = false;
+    isGrayScale = false;
 });
+
+
 
 
 // Input canvas size from user
 let canvas = document.querySelector('main .container');
 let input = document.querySelector('main .field input');
+let canvasSize = document.querySelector('main .slider-value span');
+
 createBoxes(input.value);
 color();
 input.oninput = () =>{

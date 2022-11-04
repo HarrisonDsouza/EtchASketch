127337b9333e58a9c34b33d0ec47aece29f95916
Harrison <harrisondsouza76@gmail.com>
2022-11-04 11:05:31 +0530

    Color canvas as user clicks and drags within canvas

--- script.js
+++ script.js
@@ -6,19 +6,49 @@ function createBoxes(count){
         canvas.appendChild(div);
     }
     
-    let nodes = document.querySelectorAll('main .container > *');
-    nodes.forEach(node => {
-        node.style.flexBasis = `${100/side}%`;
+    let boxes = document.querySelectorAll('main .container > *');
+    
+    boxes.forEach(box => {
+        box.style.flexBasis = `${100/side}%`;
     })
+}
+
+// paint boxes on click
+function color(){
+    let boxes = document.querySelectorAll('main .container > *')
+
+    let isDown = false;
+
+
+    for (let box of boxes){
+        box.addEventListener('mousedown', () => {
+            box.classList.add('clicked');
+            isDown = true;
+        })
+    }
+    for (let box of boxes){
+        box.addEventListener('mouseup', () => {
+            isDown = false;
+        })
+    }
+    for (let box of boxes){
+        box.addEventListener('mouseover', () => {
+            if(isDown){
+                box.classList.add('clicked');
+            }
+        })
+    }
 }
 
 // Input canvas size from user
 let canvas = document.querySelector('main .container');
 let input = document.querySelector('main .field input');
 createBoxes(input.value);
+color();
 input.oninput = () =>{
     while(canvas.firstElementChild){
         canvas.removeChild(canvas.lastElementChild);  
     }
     createBoxes(input.value);
+    color();
 }
